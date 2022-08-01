47fa31fd39c8cfa49f3cf6da8a9eba132b67c0ca
joan <https://github.com/wakander-joan/Teste.git>
2022-08-01 16:08:10 -0300

    implementa controller que implementa ClienteAPI

---
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -0,0 +1,19 @@
+package br.com.petz.clientepet.cliente.application.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@Log4j2
+public class ClienteController implements ClienteAPI {
+
+	@Override
+	public ClienteResponse postCliente(ClienteRequest clienteRequeste) {
+		log.info("[inicia] ClienteController - postCliente");
+		log.info("[inicia] ClienteController - postCliente");
+
+		return null;
+	}
+
+}
